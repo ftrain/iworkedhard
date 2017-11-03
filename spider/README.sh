bd7c5784a837672506154dbeadf5139371559ac2
Paul Ford <ford@ftrain.com>
2017-11-02 23:18:02 -0400

    Tweaks for concurrency, some comments.

--- spider/README.sh
+++ spider/README.sh
@@ -1,9 +1,7 @@
-# We will use this program, it seems fine:
-# https://github.com/hartator/wayback-machine-downloader
+#!/bin/bash
 
-# wayback_machine_downloader ${URL} --directory /Users/ford/Dropbox/mirror/ --concurrency 10 --exclude "/\.(gif|jpg|jpeg|js|mp4|mov|flv|avi|css|pdf)$/i"
-
-# And we will download
+# We want to download some websites from the Internet archive. They
+# are:
 
 for URL in "shanghaiist.com" \
 	       "dcist.com" \
@@ -12,8 +10,19 @@ for URL in "shanghaiist.com" \
 	       "sfist.com" \
 	       "dnainfo.com" \
 	       "gothamist.com";
+
+	   # We will use this program, it seems fine:
+	   # https://github.com/hartator/wayback-machine-downloader
+
+	   # Some probing shows that the Internet Archive is cool with
+	   # about 5 concurrent connections but after that you're
+	   # pushing it.
+
 do wayback_machine_downloader ${URL} \
-			      --directory /Users/ford/Dropbox/mirror/ \
-			      --concurrency 10 \
+			      --directory /Users/ford/Dropbox/mirror/${URL} \
+			      --concurrency 5 \
 			      --exclude "/\?|\.(gif|jpg|jpeg|png|js|mp4|mov|flv|avi|css|pdf)$/i";
 done;
+
+# That's all.
+# We are done so now we can make a database and a website.
